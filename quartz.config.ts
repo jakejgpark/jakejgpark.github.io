a1c67b7e43553c6391c084993375ec2ab350c67d
Jake Jonggeun Park <ndamin221@gmail.com>
2026-06-26 20:53:32 +0900

    Home: Blog Title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jonggeun's Workspace",
+    pageTitle: "JG's Workspace",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
